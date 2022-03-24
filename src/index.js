fbf3d358065f48ec18f6087be4dcba68116c417e
Have Samuel <samiehave9@gmail.com>
2022-03-24 21:07:54 +0300

    Edited Two JS and HTML files

--- src/index.js
+++ src/index.js
@@ -1,33 +1,19 @@
-import './style.css';
+import './main.scss';
+import './asset/icons/sprite.svg';
+import { addTask } from './modules/controlList';
 
-const toDoList = [];
+const form = document.querySelector('.list-container__list-content');
 
-const todoListContainer = document.getElementById('todoList-container');
+addTask(form);
 
-for (let k = 0; k < toDoList.length; k += 1) {
-  todoListContainer.innerHTML = `
-  <div class= "container">
-  <div id ="head-wrapper">
-    <h3 id = "head-text">Today's To Do</h3>
-    <i id = "top-icon" class ="fa-solid fa-arrows-rotate"></i>
-  </div>
-  <div id = "message-wrapper">
-  <p id = "msg-text">Add to your list...</p>
-  <i id="msg-icon" class = "fa-solid fa-elliipsis-vertical"></i>
-  </div>
+const checkbox = document.querySelectorAll('input[type=checkbox]');
 
-  <ul id = "items-container">
-    <div class = "item-container">
-      <li class = "li-class"><i class= "fa-regular fa-square square"></i>${toDoList[0].description}</li>
-      <i class = "fa-solid fa-ellipsis-vertical dots-icon"></i>
-    </div>
-    <div class = "item-container">
-      <li class = "li-class"><i class = "fa-regular fa-square square"></i>${toDoList[1].description}</li>
-      <i class = "fa-solid fa-ellipsis-vertical dots-icon"></i>
-    </div>
-  </ul>
-  </div>
-  <div>
-    <p id = "last-msg">Clear all completed</p>
-  </div>`;
-}
+const checkMark = (input) => {
+  input.addEventListener('change', () => {
+    if (input.checked) {
+      input.nextElementSibling.classList.add('checked');
+    } else input.nextElementSibling.classList.remove('checked');
+  });
+};
+
+checkbox.forEach((e) => checkMark(e));
